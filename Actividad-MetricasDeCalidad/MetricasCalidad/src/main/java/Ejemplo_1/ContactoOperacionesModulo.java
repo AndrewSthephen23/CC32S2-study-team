43ef470943ea4725f93f8a4381f8f168a541d556
AndrewSthephen23 <andrei3elcausita@gmail.com>
2024-05-08 10:18:06 -0500

    se agrega actividad Metricas de Calidad

---
+++ Actividad-MetricasDeCalidad/MetricasCalidad/src/main/java/Ejemplo_1/ContactoOperacionesModulo.java
@@ -0,0 +1,7 @@
+package Ejemplo_1;
+class ContactoOperacionesModulo {
+    public void enviarMensaje(String mensaje, Usuario usuario) {
+        System.out.println("Enviando mensaje a " + usuario.getNombre() + ": " + mensaje);
+    }
+}
+//ce= 1 , ca=1
---
+++ Actividad-MetricasDeCalidad/MetricasCalidad/src/main/java/Ejemplo_1/Usuario.java
@@ -0,0 +1,13 @@
+package Ejemplo_1;
+class Usuario {
+    private String nombre;
+    public Usuario(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+}
+// Ca = 4
+// Ce = 0
+
